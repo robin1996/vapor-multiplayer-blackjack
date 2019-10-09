8fccbc292940a5360ad64e7057715ed3c78ad775
Robin Douglas <robin.1996@live.co.uk>
2019-10-09 15:36:28 +0100

    Add end game

--- Sources/App/Controllers/GameController.swift
+++ Sources/App/Controllers/GameController.swift
@@ -17,9 +17,14 @@ class GameController {
     private let gameLoop = MultiThreadedEventLoopGroup(numberOfThreads: 1).next()
     private var deck: Deck = []
     private var clients: Clients
-    private var turn = 0
+    private var turn = 0 {
+        didSet {
+            round = roundFor(turn: turn)
+        }
+    }
+    private var round = 0
     private var client: ClientController {
-        return clients[Int(Double(turn).truncatingRemainder(dividingBy: Double(clients.count)))]
+        return clients[Int(turn % clients.count)]
     }
     weak var delegate: GameControllerDelegate?
 
@@ -29,7 +34,9 @@ class GameController {
     }
 
     func start() {
+        turn = 0
         deck = Deck.standard()
+        clearHands()
         delegate?.gameStarted(with: clients, gameController: self)
         takeStake()
     }
@@ -73,7 +80,16 @@ class GameController {
         let hand = client.player.hands[0]
         guard handStillInPlay(hand) else {
             print("⏩ Skipping \(client.player.username)")
-            nextTurn()
+            if roundFor(turn: turn + 1) > round && isGameOver() { // `roundFor`
+                                                                 // stops `isGa-
+                                                                 // meOver` from
+                                                                 // being called
+                                                                 // more than o-
+                                                                 // nce a round.
+                completeGame()
+            } else {
+                nextTurn()
+            }
             return
         }
         guard hand.cards.count >= 2 else {
@@ -115,9 +131,31 @@ class GameController {
         })
     }
 
+    private func completeGame() {
+        clients.forEach { (client) in
+            let hand = client.player.hands[0]
+            _ = try! client.request(
+                actions: [],
+                withType: hand.lowTotal <= 21 ? .win : .lose,
+                onLoop: self.gameLoop
+            )
+        }
+        gameLoop.scheduleTask(in: TimeAmount.seconds(5)) { [weak self] in
+            guard let self = self else { print("☢️ GAME DEAD ☢️"); return }
+            self.clients.forEach({ _ = try! $0.request(
+                actions: [],
+                withType: .ended,
+                onLoop: self.gameLoop
+            ) })
+            self.gameLoop.scheduleTask(in: TimeAmount.seconds(2)) { [weak self] in
+                self?.start()
+            }
+        }
+    }
+
     // MARK: Actions
 
-    func hit() {
+    private func hit() {
         let hand = client.player.hands[0]
         hand.cards.append(self.deck.drawCard())
         try! client.request(
@@ -129,7 +167,7 @@ class GameController {
         }
     }
 
-    func stand() {
+    private func stand() {
         let hand = client.player.hands[0]
         hand.hasStood = true
         try! client.request(
@@ -143,7 +181,7 @@ class GameController {
 
     // MARK: Helpers
 
-    func nextTurn() {
+    private func nextTurn() {
         self.turn += 1
         self.takeTurn()
     }
@@ -152,19 +190,28 @@ class GameController {
     ///
     /// - Parameter hand: Hand to check.
     /// - Returns: True if the hand is still in play.
-    func handStillInPlay(_ hand: Hand) -> Bool {
-//        return hand.lowTotal < 21 && !hand.hasStood
-        return true
+    private func handStillInPlay(_ hand: Hand) -> Bool {
+        return hand.lowTotal < 21 && !hand.hasStood
     }
 
     /// Checks if the game is over.
     ///
     /// - Returns: True if the game is over.
-    func isGameOver() -> Bool {
+    private func isGameOver() -> Bool {
         return !clients.reduce(false) { (inPlay, client) -> Bool in
             inPlay || client.player.hands.reduce(false, { (inPlay, hand) -> Bool in
                 inPlay || self.handStillInPlay(hand)
             })
+        }
+    }
+
+    private func roundFor(turn: Int) -> Int {
+        return Int(floor(Double(turn) / Double(clients.count)))
+    }
+
+    private func clearHands() {
+        clients.forEach { (client) in
+            client.player.hands = []
         }
     }
 
